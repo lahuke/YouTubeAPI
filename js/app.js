bef3a33e41bcba039f75bf9db22302436c2c5f1a
lahuke <luke@luketucker.com>
2017-10-27 20:43:06 -0600

    sort of

--- js/app.js
+++ js/app.js
@@ -1,66 +1,39 @@
-var YouTubeSearchURL = 'https://www.googleapis.com/youtube/v3/search';
+$(document).ready(function(){
+  
+  $(function() {
+      $('.js-search-form').submit(function (event) {
+        event.preventDefault();
+        var searchTerm = $('.js-query').val();
+        var queryTarget = $(event.currentTarget).find('.js-query');
+        var query = queryTarget.val();
+        getRequest(searchTerm);
+      });
+  });
 
-function getDataFromApi(searchTerm, callback) {
-  var query = {
-  	part: 'snippet',
-    key: 'AIzaSyACK3imt8Ow7yrWE8S4o-viM-Da22u5OtU',
-    q: searchTerm + " in:title",
-    per_page: 5
+  function getRequest(searchTerm) {
+      var query = {
+          part: 'snippet',
+          key: 'AIzaSyACK3imt8Ow7yrWE8S4o-viM-Da22u5OtU',
+          q: searchTerm,
+          type: 'video',
+          per_page:5
+      };
+      var url = 'https://www.googleapis.com/youtube/v3/search';
+
+      $.getJSON(url, query, function (data) {
+          showResults(data.items);
+      });
   }
-  $.getJSON(YouTubeSearchURL, query, callback);
-};
 
-function renderResult(result) {
-  var template = $(RESULT_HTML_TEMPLATE);
-  template.find(".js-result-thumbnail").image(result.thumbnails.default).attr("href", result.thumbnails.url);
-  return template;
-}
+  function showResults(results){
+      var html = "";
 
-var RESULT_HTML_TEMPLATE = (
-  '<div>' +
-    '<span class="js-result-thumbnail"></span>' +
-  '</div>'
-);
-
-//Copied from sample. 
-//Still need to update all content following this marker.
-
-var RESULT_HTML_TEMPLATE = (
-  '<div>' +
-    '<h2>' +
-    '<a class="js-result-name" href="" target="_blank"></a> by <a class="js-user-name" href="" target="_blank"></a></h2>' +
-    '<p>Number of watchers: <span class="js-watchers-count"></span></p>' + 
-    '<p>Number of open issues: <span class="js-issues-count"></span></p>' +
-  '</div>'
-);
+      $.each(results, function(index, value){
+        $('<img>').attr("src", value.snippet.thumbnails.medium.url).appendTo('body');
+        console.log(value.snippet.thumbnails.medium.url);
+        $(".js-query").val("");
+    });
+  }
+}); 
 
 
-
-function renderResult(result) {
-  var template = $(RESULT_HTML_TEMPLATE);
-  template.find(".js-result-name").text(result.name).attr("href", result.html_url);
-  template.find(".js-user-name").text(result.owner.login).attr("href", result.owner.html_url);
-  template.find(".js-watchers-count").text(result.watchers_count);
-  template.find(".js-issues-count").text(result.open_issues);
-  return template;
-}
-
-function displayGitHubSearchData(data) {
-  var results = data.items.map(function(item, index) {
-    return renderResult(item);
-  });
-  $('.js-search-results').html(results);
-}
-
-function watchSubmit() {
-  $('.js-search-form').submit(function(event) {
-    event.preventDefault();
-    var queryTarget = $(event.currentTarget).find('.js-query');
-    var query = queryTarget.val();
-    // clear out the input
-    queryTarget.val("");
-    getDataFromApi(query, displayGitHubSearchData);
-  });
-}
-
-$(watchSubmit);
